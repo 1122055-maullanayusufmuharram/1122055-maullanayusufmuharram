3ad11c3b090fc63d8e42611f91da4defde3da00e
1122055-maullanayusufmuharram <150674339+1122055-maullanayusufmuharram@users.noreply.github.com>
2023-11-13 03:19:35 -0800

    Add files via upload

---
+++ galeri.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title>Maullana-11220055</title>
+<head>
+<body>
+	<?php
+	echo"<table border = '1' width = '100%' >";
+		echo"<tr>";
+			echo"<th colspan='2' height='50%'> <img src = 'g1.jpg' width ='100%' height='100'/> </th>";
+		echo"</tr>";
+		
+		echo"<tr height='2000'>";
+			echo"<td width = '20%'><table border = '0' width = '100%'>
+									<tr>
+									<th>Menu</th>
+									</tr>
+									<tr>
+									<th><a href = 'index.php'>Beranda</a></th>
+									</tr>
+									<tr>
+									<th><a href = 'CV.php'>CV</a></th>
+									</tr>
+									<tr>
+									<th><a href = 'Galeri.php'>Galeri</a></th>
+									</tr>
+			</table></td>";
+			echo"<td>
+			<img src = 'g3.jpg' width ='100%' height='2000'/>
+			</td>";
+		echo"</tr>";
+		
+		echo"<tr>";
+		echo"<td colspan='3' bgcolor='cyan' width = '20%'><table border = '0' width = '100%'>
+									<tr>
+									<td width = '20%'>dibuat oleh</td>
+									<td >Tujuan</td>
+									</tr>
+									<tr>
+									<td >Maullana Yusuf Muharram</td>
+									<td >melatih kemampuan dalam penggunakan bahasa programing</td>
+									</tr>
+									<tr>
+									<td >Mahasiswa STMIK DCI</td>
+									</tr>
+									<tr>
+									<td >angkatan 22</td>
+									</tr>
+			</table></td>";
+		echo"</tr>";
+	echo"</table>";
+	?>
+</body>
+</html> 
